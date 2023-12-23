82f2e30498672436e8ad3971d56f9a124e491659
manhdat.tang <tangdatsdbg@gmail.com>
2023-12-23 16:41:37 +0700

    deploy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -29,6 +29,6 @@
     </head>
     <body>
         <div id="app"></div>
-        <script src="{{ asset('js/index.js') }}"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
